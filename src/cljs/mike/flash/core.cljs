3193bd3f65d6990eb255d6284f8f025ebe128cce
Mike <mike706574@gmail.com>
2015-11-02 00:05:54 -0600

    stuff

--- src/cljs/mike/flash/core.cljs
+++ src/cljs/mike/flash/core.cljs
@@ -27,23 +27,21 @@
 (defn load-lessons!
   [state type-id]
   (commit! state :loading-lessons? true)
-  (go
-    (let [{:keys [status body message]} (<! (api/get-lessons repo type-id))] 
-      (if (joe/ok? status)
-        (commit! state :loading-lessons? false :lessons body)
-        (error! state message)))))
+  (go (let [{:keys [status body message]} (<! (api/get-lessons repo type-id))]
+        (if (joe/ok? status)
+          (commit! state :loading-lessons? false :lessons body)
+          (error! state message)))))
 
 (defn load-sessions!
   [state type-id]
   (commit! state :loading-sessions? true)
-  (go
-    (let [{:keys [status body message]} (<! (api/get-sessions repo type-id))] 
-      (if (joe/ok? status)
-        (let [sessions (group-by :done body)]
-          (merge! state {:loading-sessions? false
-                         :active-sessions (get sessions false)
-                         :completed-sessions (get sessions true)})) 
-        (error! state message)))))
+  (go (let [{:keys [status body message]} (<! (api/get-sessions repo type-id))] 
+        (if (joe/ok? status)
+          (let [sessions (group-by :done body)]
+            (merge! state {:loading-sessions? false
+                           :active-sessions (get sessions false)
+                           :completed-sessions (get sessions true)})) 
+          (error! state message)))))
 
 (defn answer-question!
   [state correct?]
@@ -51,7 +49,6 @@
   (go
     (let [{:keys [type-id session]} @state
           {:keys [id entity-id]} session
-          _ (println type-id id entity-id)
           {:keys [status body message]} (<! (api/record-answer! repo type-id id entity-id correct?))] 
       (if (joe/ok? status)
         (done! state :session body)
@@ -66,70 +63,91 @@
 (defn load-types!
   [state]
   (loading! state)
-  (go
-    (let [{:keys [status body message]} (<! (api/get-types repo))]
-      (if (joe/ok? status) 
-        (if (empty? body)
-          (fatal! state "No types found!")
-          (let [type-id (key (first body))]
-            (commit! state :types body)
-            (load-type! state type-id)))
-        (fatal! state message)))))
+  (go (let [{:keys [status body message]} (<! (api/get-types repo))]
+        (if (joe/ok? status) 
+          (if (empty? body)
+            (fatal! state "No types found!")
+            (let [type-id (key (first body))]
+              (commit! state :types body)
+              (load-type! state type-id)))
+          (fatal! state message)))))
 
-(defn session!
-  [state session-id]
-  (println "LOADING SESSION" session-id)
+(defn resolve-arg
+  [state arg]
+  (cond (keyword? arg) (get state arg)
+        (vector? arg) (get-in state arg)
+        :else arg))
+
+(defn load!
+  [state k f args]
   (loading! state)
-  (go (let [{:keys [type-id lesson-id]} @state
-            {:keys [status body message]} (<! (api/get-session repo type-id session-id))]
-        (if (joe/bad? status)
-          (error! state message)
-          (let [session body
-                lesson-id (:lesson-id session)
-                {:keys [status body]} (<! (api/get-lesson repo type-id lesson-id))]
-            (if (joe/bad? status)
-              (error! state message)
-              (done! state :session session :lesson body) 
-))))))
+  (go (let [args (map #(resolve-arg state %) args)
+            {:keys [status body message]} (<! (apply f args))]
+        (if (joe/ok? status)
+          (done! state k body)
+          (error! state message)))))
+
+(def third #(nth % 2))
+
+;; TODO: this is sort of crazy?
+(defn in-order!
+  [state calls]
+  (loading! state)
+  (go (loop [remaining-calls calls
+             updated @state
+             changes {}]
+        (if (empty? remaining-calls)
+          (swap! state merge (assoc changes :loading false :error false))
+          (let [call (first remaining-calls)
+                k (first call)
+                ;; TODO: kind of hacky here
+                {:keys [status body message]} (if (keyword? (second call))
+                                                {:status :ok :body (second call)}
+                                                (let [f (second call)
+                                                      template (third call) 
+                                                      args (map #(resolve-arg updated %) template)]
+                                                  (<! (apply f args))))]
+            (if (joe/ok? status)
+              (recur (rest remaining-calls) (assoc updated k body) (assoc changes k body))
+              (error! state message)))))))
 
 (defn create-session!
   [state lesson-id]
-  (commit! state :creating-session? true)
-  (go
-    (let [{:keys [type-id]} @state
-          {:keys [status body message]} (<! (api/create-session! repo type-id lesson-id))]
-      (if (joe/ok? status)
-        (let [session-id (:id body)]
-          (session! state session-id)
-          (commit! state :mode :session :creating-session? false))
-        (error! state message)))))
+  (in-order! state [[:session api/create-session! [repo :type-id lesson-id]]
+                    [:lesson api/get-lesson [repo :type-id [:session :lesson-id]]]
+                    [:entity api/get-entity [repo :type-id [:session :entity-id]]]
+                    [:mode :session]]))
+
+(defn session!
+  [state session-id]
+  (in-order! state [[:session api/get-session [repo :type-id session-id]]
+                    [:lesson api/get-lesson [repo :type-id [:session :lesson-id]]]
+                    [:entity api/get-entity [repo :type-id [:session :entity-id]]]
+                    [:mode :session]]))
 
 (defn render-session
   [state]
   (let [{:keys [type-id lesson loading-session? loading-lesson? loading-type? session entity attribute]} @state]
-    (if (or loading-lesson? loading-session? loading-type?)
-      [:span "Loading..."]
-      (let [{:keys [name length entities]} lesson
-            {:keys [id correct total done entity-id]} session]
-        [:div
-         (com/button :to-sessions "Back to Sessions" (fn []
-                                                       (load-type! state type-id)
-                                                       (commit! state :mode :sessions)))
-         [:p "Session ID: " id]
-         [:h3 "Lesson: " name]
-         [:p correct " correct out of " total] 
-         (if done
-           [:div
-            [:p "DONE"]]
-           [:div
-            [:p "Playing to " length] 
-            [:p "Entity ID:" entity-id]
-            (com/button :correct "Correct" (fn [] (answer-question! state true))) 
-            (com/button :wrong "Wrong" (fn [] (answer-question! state false))) 
-            (com/button :new-question "Back to Sessions" (fn []
-                                                           (load-type! state type-id)
-                                                           (commit! state :mode :sessions)))]
-           )]))))
+    (let [{:keys [name length entities]} lesson
+          {:keys [id correct total done entity-id]} session]
+      [:div
+       (com/button :to-sessions "Back to Sessions" (fn []
+                                                     (load-type! state type-id)
+                                                     (commit! state :mode :sessions)))
+       [:p "Session ID: " id]
+       [:h3 "Lesson: " name]
+       [:p correct " correct out of " total] 
+       (if done
+         [:div
+          [:p "DONE"]]
+         [:div
+          [:p "Playing to " length] 
+          [:p "Entity ID:" entity-id]
+          (com/button :correct "Correct" (fn [] (answer-question! state true))) 
+          (com/button :wrong "Wrong" (fn [] (answer-question! state false)))])
+       (com/button :new-question "Back to Sessions" (fn []
+                                                      (load-type! state type-id)
+                                                      (commit! state :mode :sessions)))])))
 
 (defn render-sessions
   [state]
@@ -148,8 +166,7 @@
                               [:property :user]
                               [:property :name] 
                               [:property :description]
-                              [:action :start "Start" create-session! [state :id]]]))])
-
+                              [:action :start "Start" create-session! [state :id]]]))]) 
      (if loading-sessions
        [:p "Loading sessions..."]
        [:div 
@@ -160,20 +177,17 @@
                                       [:property :lesson-id]
                                       [:property :correct]
                                       [:property :total]
-                                      [:action :continue "Continue" (fn [{id :id}]
-                                                                      (session! state id)
-                                                                      (commit! state :mode :session))]]))
+                                      [:action :continue "Continue"
+                                       (fn [{id :id}]
+                                         (session! state id)
+                                         (commit! state :mode :session))]]))
         [:h2 "Completed Sessions"]
         (if (empty? completed-sessions)
           [:span "No completed sessions."]
           (com/table completed-sessions [[:property :id]
                                          [:property :lesson-id]
                                          [:property :correct]
-                                         [:property :total]]))
-
-        ])
-
-     ]))
+                                         [:property :total]]))])]))
 
 (defn app
   []
